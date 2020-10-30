eecbe3d0a11c0a429de075efd697220ab60d3903
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-10-30 22:04:28 +0700

    create navbar

---
+++ components/VNavbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-navbar>
+    <b-navbar-brand href="#">
+      <img src="@/static/edgeprop.png" alt="edgeprop logo" class="img-logo" />
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/" class="mr-48" exact exact-active-class="active"
+          >Home</b-nav-item
+        >
+        <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.img-logo {
+  width: 197.67px;
+}
+.nav-link {
+  font-size: 20px;
+  &.active {
+    color: 333333, 100%;
+    font-weight: 600;
+    border-bottom: 3px solid #1a7cd2;
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,31 +4,26 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'app_repository_fe',
+    title: 'Apps Reposiroty',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: ['@/assets/style.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
@@ -44,6 +39,5 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {},
 }
